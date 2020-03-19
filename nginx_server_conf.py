048bbc16661c471577ec804880c0541dbff9bcc8
patrick <patrickwuke@163.com>
2020-03-19 10:41:43 +0800

    update nginx configuration

--- nginx_server_conf.py
+++ nginx_server_conf.py
@@ -7,6 +7,7 @@ from nginx.config.api import Location
 class NginxTemplate(Template):
     delimiter = "%"
 
+
 # todo: add if there is no configuration file
 def load_nginx_configuration():
     with open("nginx_setting.json", "r") as setting:
@@ -15,15 +16,14 @@ def load_nginx_configuration():
 
 
 nginx_settings = load_nginx_configuration()
-default_root_path = "/var/www"
 NGINX_CONF_FILE = "nginx.conf"
 
 
 def update_nginx_setting(project_name):
     nginx_setting = {
-        "root": default_root_path + "/" + project_name,
-        "try_files": "$uri $uri / / index.html",
-        "index": "index.html"
+        "try_files": "$uri $uri/ @router",
+        "index": "index.html",
+        "router": "rewrite ^.*$ /{project_name}/index.html last;".format(project_name=project_name)
     }
     nginx_settings["locations"][project_name] = nginx_setting
     write_to_nginx_setting(nginx_settings)
@@ -37,20 +37,20 @@ def write_to_nginx_setting(nginx_json_setting):
 def append_location_to_nginx(project_name):
     update_nginx_setting(project_name=project_name)
     location_str = []
+    routers = []
     for project_name, setting in nginx_settings["locations"].items():
-        location = Location("/" + project_name, root=setting["root"]
-                            , try_files=setting["try_files"],
-                            index=setting["index"])
+        location = Location("/" + project_name, try_files=setting["try_files"],index = setting["index"])
         location_str.append(str(location))
-
-    generate_server_by_locations("".join(location_str))
+        routers.append(str(setting['router']))
+    generate_confs(routers="\n\t\t   ".join(routers), locations ="\t\t".join(location_str))
     return NGINX_CONF_FILE
 
-def generate_server_by_locations(locations):
+
+def generate_confs(routers, locations):
     with open('server.conf', 'r', encoding='utf-8') as conf:
         lines = conf.readlines()
         conf_temp = NginxTemplate("".join(lines))
-        conf_content = conf_temp.substitute(locations=locations)
+        conf_content = conf_temp.substitute(locations=locations, routers=routers)
 
     with open(NGINX_CONF_FILE, 'w', encoding="utf-8") as new_conf:
         new_conf.write(conf_content)
